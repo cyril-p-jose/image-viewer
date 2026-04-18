d729ebe36a7b1f755c6030dc2429163d1dea8b41
CYRIL P JOSE <cyrilpjose2025@gmail.com>
2026-04-18 21:53:44 +0530

    Update image source paths in nextImage function

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function nextImage() {
     let img = document.getElementById("image");
 
     if (currentImage === 1) {
-        img.src = "https://github.com/cyril-p-jose/image-viewer/blob/main/sjcet%20nightview.jpeg";
+        img.src = "sjcet nightview.jpeg";
         currentImage = 2;
     } else {
         img.src = "https://github.com/cyril-p-jose/image-viewer/blob/main/sjcet%20daylight.jpeg";
